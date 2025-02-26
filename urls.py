5f697e0abc986ea90c50085443eb720ecbc014ba
EdisonValdez <eddynvg@hotmail.com>
2025-02-26 19:44:01 +0100

    Health check in urls.py

--- urls.py
+++ urls.py
@@ -6,8 +6,17 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from wagtail import urls as wagtail_urls
 from search import views as search_views
+from django.http import HttpResponse
+from django.urls import path, include
+
+def health_check(request):
+    """
+    Simple view that returns a 200 OK response for health checks.
+    """
+    return HttpResponse("OK", status=200)
 
 urlpatterns = [
+    path('', include('lscms.urls')),   
     path('django-admin/', admin.site.urls),
     path('admin/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
